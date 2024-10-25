51e5f7c361f163b98965f6f2fdb6c45619d8a063
CHy-KK <ychanjusuki@gmail.com>
2024-10-25 14:35:34 +0000

    langchain success

--- openaiTest.py
+++ openaiTest.py
@@ -7,6 +7,7 @@ from langchain.schema import (
 )
 
 
+
 langchain_messages = [
     SystemMessage(content="You are a helpful assistant."),
     HumanMessage(content="Knock knock."),
@@ -14,15 +15,14 @@ langchain_messages = [
     HumanMessage(content="Orange"),
 ]
 
-print(langchain_messages)
 
 langchinChat = ChatOpenAI(
     openai_api_key="sk-OAbZ2yRJdYz9KOC207C60e1644274dCbB029B417E246E851",
-    model='gpt-3.5-turbo'
+    model='gpt-3.5-turbo',
+    base_url="https://api3.apifans.com/v1"
 )
-print(langchinChat)
 
-res = langchinChat(langchain_messages)
 print('-------------------------------')
-print (res)
+res = langchinChat(langchain_messages)
+print (res.content)
 print('-------------------------------')
--- ragutils.py
+++ ragutils.py
@@ -1,22 +1,46 @@
 from openai import OpenAI
-
-client = OpenAI(
-    api_key="sk-OAbZ2yRJdYz9KOC207C60e1644274dCbB029B417E246E851",
-    base_url="https://api3.apifans.com/v1"
+from langchain_openai import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
 )
+import os
 
-def pushUserMessage(msgs: list, content: str):
-    msgs.append({'role':'user', 'content': content})
+os.environ["OPENAI_API_KEY"] = "sk-OAbZ2yRJdYz9KOC207C60e1644274dCbB029B417E246E851"
 
-def getGptMsg(msgs:list):
-    '''返回gpt的回复并自动加入当前对话列表'''
-    completion = client.chat.completions.create(
-        messages=msgs,
-        model="gpt-3.5-turbo-1106",
-    )
-    receiveMsg = completion.choices[0].message.content
-    msgs.append({'role':'system', 'content': receiveMsg})
-    return receiveMsg
+class ChatGpt:
+    def __init__(self) -> None:
+        self.client = OpenAI(
+            api_key=os.environ["OPENAI_API_KEY"],
+            base_url="https://api3.apifans.com/v1"
+        )
+        self.msgs = []
 
-def deleteMessage(msgs:list):
-    msgs.pop()
+
+    def pushUserMessage(self, content: str):
+        self.msgs.append({'role':'user', 'content': content})
+        
+    def pushSystemMessage(self, content: str):
+        self.msgs.append({'role':'system', 'content': content})
+
+    def getGptMsg(self):
+        '''返回gpt的回复并自动加入当前对话列表'''
+        completion = self.client.chat.completions.create(
+            messages=self.msgs,
+            model="gpt-3.5-turbo-1106",
+        )
+        receiveMsg = completion.choices[0].message.content
+        self.msgs.append({'role':'system', 'content': receiveMsg})
+        return receiveMsg
+
+    def deleteMessage(self):
+        self.msgs.pop()
+
+class ragChat:
+    def __init__(self) -> None:
+        self.langchinChat = ChatOpenAI(
+            openai_api_key="sk-OAbZ2yRJdYz9KOC207C60e1644274dCbB029B417E246E851",
+            model='gpt-3.5-turbo',
+            base_url="https://api3.apifans.com/v1"
+        )
