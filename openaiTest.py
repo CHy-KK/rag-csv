339c2a55613c2c921374edfc7e94d3d85af04515
CHy-KK <ychanjusuki@gmail.com>
2024-10-29 14:53:13 +0000

    rag docs

--- openaiTest.py
+++ openaiTest.py
@@ -1,28 +1,25 @@
-from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+os.environ["OPENAI_API_KEY"] = "sk-OAbZ2yRJdYz9KOC207C60e1644274dCbB029B417E246E851"
+loader = PyPDFLoader("Baichuan2.pdf")
 
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
+pages = loader.load_and_split()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50,
 )
 
+docs = text_splitter.split_documents(pages)
 
-
-langchain_messages = [
-    SystemMessage(content="You are a helpful assistant."),
-    HumanMessage(content="Knock knock."),
-    AIMessage(content="Who's there?"),
-    HumanMessage(content="Orange"),
-]
-
-
-langchinChat = ChatOpenAI(
-    openai_api_key="sk-OAbZ2yRJdYz9KOC207C60e1644274dCbB029B417E246E851",
-    model='gpt-3.5-turbo',
-    base_url="https://api3.apifans.com/v1"
-)
-
-print('-------------------------------')
-res = langchinChat(langchain_messages)
-print (res.content)
-print('-------------------------------')
+print(len(docs))
+embed_model = OpenAIEmbeddings(model="text-embedding-3-large", base_url="https://api3.apifans.com/v1")
+vectors = embed_model.embed_documents(["hello", "goodbye"])
+# Showing only the first 3 coordinates
+print(len(vectors))
+print(vectors[0][:3])
+# embed_model
+# vectorstore = Chroma.from_documents(documents=docs, embedding=embed_model, collection_name="openai_embed")
